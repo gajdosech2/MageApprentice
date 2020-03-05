86056834488d3a07748cf77e26bf89dd8191c422
gajdosech2 <l.gajdosech@gmail.com>
2020-03-05 12:29:52 +0100

    Elixirs!
    Former-commit-id: 6ff2eebdbc8cdea99a594cb7933b5a6b0c9f275b

---
+++ Assets/Scripts/Game/Elixir.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elixir : MonoBehaviour
+{
+    public GameObject message;
+    public List<GameObject> elixirs;
+    bool interact = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") && interact)
+        {
+            for (Types i = Types.red; i <= Types.none; i++)
+            {
+                if (i == Types.none)
+                {
+                    Types drop = Elixirs.instance.Drop();
+                    if (drop != Types.none)
+                    {
+                        elixirs[(int)drop].SetActive(true);
+                    }
+                }
+                else if (elixirs[(int)i].active)
+                {
+                    elixirs[(int)i].SetActive(false);
+                    Types pop = Elixirs.instance.Add(i);
+                    if (pop != Types.none)
+                    {
+                        elixirs[(int)pop].SetActive(true);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        message.SetActive(true);
+        interact = true;
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        message.SetActive(false);
+        interact = false;
+    }
+}
---
+++ Assets/Scripts/Game/Elixirs.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Types { red, green, blue, white, none };
+
+public class Elixirs : MonoBehaviour
+{
+    public static Elixirs instance;
+    public List<GameObject> slot1_elixirs;
+    public List<GameObject> slot2_elixirs;
+    Types player_slot1 = Types.none;
+    Types player_slot2 = Types.none;
+
+    void Refresh()
+    {
+        for (Types i = Types.red; i < Types.none; i++)
+        {
+            if (player_slot1 == i)
+            {
+                slot1_elixirs[(int)i].SetActive(true);
+            }
+            else
+            {
+                slot1_elixirs[(int)i].SetActive(false);
+            }
+
+            if (player_slot2 == i)
+            {
+                slot2_elixirs[(int)i].SetActive(true);
+            }
+            else
+            {
+                slot2_elixirs[(int)i].SetActive(false);
+            }
+        }
+    }
+
+    public Types Drop()
+    {
+        Types ret = Types.none;
+        if (player_slot2 != Types.none)
+        {
+            ret = player_slot2;
+            player_slot2 = Types.none;
+        }
+        else if (player_slot1 != Types.none)
+        {
+            ret = player_slot1;
+            player_slot1 = Types.none;
+        }
+        return ret;
+    }
+
+    public Types Add(Types elixir)
+    {
+        if (player_slot1 == Types.none)
+        {
+            player_slot1 = elixir;
+            return Types.none;
+        }
+        if (player_slot2 == Types.none)
+        {
+            player_slot2 = player_slot1;
+            player_slot1 = elixir;
+            return Types.none;
+        }
+        Types pop = player_slot2;
+        player_slot2 = player_slot1;
+        player_slot1 = elixir;
+        return pop;
+    }
+
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Update()
+    {
+        Refresh();
+    }
+
+}
