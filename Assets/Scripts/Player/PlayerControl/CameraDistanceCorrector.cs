7b73f072af6af76d5ff4fe520a75da445dd662e4
Adam Riecicky <riecicky@skeletex.xyz>
2020-03-21 00:04:47 +0100

    Using structure static constants for initialization.

--- Assets/Scripts/Player/PlayerControl/CameraDistanceCorrector.cs
+++ Assets/Scripts/Player/PlayerControl/CameraDistanceCorrector.cs
@@ -12,13 +12,13 @@ public class CameraDistanceCorrector : MonoBehaviour
     float max_distance;
     float current_distance;
     Vector3 local_target;
-    Vector2 near_plane_extents = new Vector2();
+    Vector2 near_plane_extents = Vector2.zero;
 
     void Start()
     {
         cam = GetComponent<Camera>();
         // warning: camera rotation must be zero, otherwise script does not work properly
-        cam.transform.rotation = new Quaternion();
+        cam.transform.rotation = Quaternion.identity;
 
         local_target = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
         max_distance = current_distance = -transform.localPosition.z;
